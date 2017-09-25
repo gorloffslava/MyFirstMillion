9b4f1f547ced18658aecaf667c06aa498d191748
Gorloff <gorloff.slava@gmail.com>
2017-09-25 16:46:12 +0600

    Initial Commit

---
+++ MyFirstMillion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstMillion
+//
+//  Created by Вячеслав Горлов on 25.09.17.
+//  Copyright © 2017 Gorloff Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
